2173987aa8947393a354c22dbc462515ef4658be
rexaevum <rolans2k@gmail.com>
2021-04-04 19:49:55 -0400

    update monotonic solution with given

--- AlgoExp/Medium/Monotonic_Array.py
+++ AlgoExp/Medium/Monotonic_Array.py
@@ -24,3 +24,16 @@ def isMonotonic(array):
 		if array[i] < array[i-1] and change == 1:
 			return False
 	return True
+
+# Time - O(n)
+# Space - O(1)
+def isMonotonicGiven(array):
+    isNonDecreasing = True
+    isNonIncreasing = True
+    for i in range (1, len(array)):
+        if array[i] > array[i-1]:
+            isNonIncreasing = False
+        if array[i] < array[i-1]:
+            isNonDecreasing = False
+    #
+    return isNonDecreasing or isNonIncreasing
